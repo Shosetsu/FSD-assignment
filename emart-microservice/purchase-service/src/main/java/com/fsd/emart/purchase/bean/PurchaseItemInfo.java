a68ba90a5d7072fed72dd5d8600f41e29360f46a
Shosetsu <Shosetsu.rr@outlook.com>
2020-05-14 00:30:27 +0800

    change function process

--- emart-microservice/purchase-service/src/main/java/com/fsd/emart/purchase/bean/PurchaseItemInfo.java
+++ emart-microservice/purchase-service/src/main/java/com/fsd/emart/purchase/bean/PurchaseItemInfo.java
@@ -1,10 +1,13 @@
 package com.fsd.emart.purchase.bean;
 
+import java.math.BigDecimal;
+
 import lombok.Data;
 
 @Data
 public class PurchaseItemInfo {
 
 	private String id;
+	private BigDecimal price;
 	private Integer count;
 }
--- emart-microservice/purchase-service/src/main/java/com/fsd/emart/purchase/service/impl/PurchaseServiceImpl.java
+++ emart-microservice/purchase-service/src/main/java/com/fsd/emart/purchase/service/impl/PurchaseServiceImpl.java
@@ -2,6 +2,7 @@ package com.fsd.emart.purchase.service.impl;
 
 import javax.annotation.Resource;
 
+import com.fsd.emart.common.dao.ItemDao;
 import com.fsd.emart.common.dao.OrderDao;
 import com.fsd.emart.purchase.bean.PurchaseItemInfo;
 import com.fsd.emart.purchase.service.PurchaseService;
@@ -11,6 +12,9 @@ public class PurchaseServiceImpl implements PurchaseService {
 	@Resource
 	private OrderDao orderDao;
 
+	@Resource
+	private ItemDao itemDao;
+	
 	@Override
 	public void purchase(PurchaseItemInfo[] purchaseList) {
 		// TODO
