d11e75b9e4d72adf5379d0d6703efeead66b5b2b
Shosetsu <Shosetsu.rr@outlook.com>
2020-05-15 03:49:29 +0800

    fix common module import

--- emart-microservice/auth-service/src/main/java/com/fsd/emart/auth/AuthApplication.java
+++ emart-microservice/auth-service/src/main/java/com/fsd/emart/auth/AuthApplication.java
@@ -2,8 +2,12 @@ package com.fsd.emart.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = { "com.fsd.emart" })
+@SpringBootApplication(scanBasePackages = "com.fsd.emart.*")
+@EnableJpaRepositories("com.fsd.emart.common.dao")
+@EntityScan("com.fsd.emart.*")
 public class AuthApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AuthApplication.class, args);
